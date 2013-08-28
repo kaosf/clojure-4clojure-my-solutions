21305d35d70b8ed1160b83cfaebdb360acafce78
ka <ka.kaosf@gmail.com>
2013-08-29 00:42:42 +0900

    modify 0026.clj (add a correct answer)

--- 0026.clj
+++ 0026.clj
@@ -3,3 +3,5 @@
     (defn fib [x] (if (or (= x 1) (= x 2)) 1 (+ (fib (- x 1)) (fib (- x 2)))))
     (map fib (map inc (range %))))
 ; This is not suitable for the answer because of using "def"!
+
+(fn [n] (map (fn fib [x] (if (or (= x 1) (= x 2)) 1 (+ (fib (- x 1)) (fib (- x 2))))) (map inc (range n))))
