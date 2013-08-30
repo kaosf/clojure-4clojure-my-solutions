436eaab41f7c1ac16fbb618ce258ff07fd003550
ka <ka.kaosf@gmail.com>
2013-08-31 01:44:04 +0900

    modify 0039.clj (increase readability)

--- 0039.clj
+++ 0039.clj
@@ -1,1 +1,4 @@
-(fn f [a b] (if (or (empty? a) (empty? b)) nil (cons (first a) (cons (first b) (f (rest a) (rest b))))))
+(fn f [a b]
+  (if (or (empty? a) (empty? b))
+    nil
+    (cons (first a) (cons (first b) (f (rest a) (rest b))))))
